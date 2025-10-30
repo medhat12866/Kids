c6989ee181a27de72a365e9e7c5a8fbdce3ce95c
medhat12866 <madhat0100677@gmail.com>
2025-10-30 21:17:16 +0300

    Update project name to "Kids" in package.json and package-lock.json; add .vercel to .gitignore; integrate Vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { Analytics } from "@vercel/analytics/next";
-
+import { inject } from "@vercel/analytics";
 createRoot(document.getElementById("root")!).render(
   <>
     <App />
-    <Analytics />
   </>
 );
+inject();
